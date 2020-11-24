eb992194a8b71f95ac8bd9a5cdec7dd8063f6346
andieay <48040105+andieay@users.noreply.github.com>
2020-11-24 18:00:20 +0800

    Add files via upload

---
+++ L01.py
@@ -0,0 +1,45 @@
+# L01
+
+# Python 3.7 or above only!
+# Fill in the table with data in hko website with mean July climate and finish the following tasks.
+# https://www.hko.gov.hk/en/cis/monthlyElement.htm
+# Save the table with as L01.csv file
+
+# ------------------------------------------------
+# Import libraries here (e.g. numpy, panda...)
+
+
+
+
+# ------------------------------------------------
+# Read the csv file and import the data.
+print("Read csv file")
+
+
+# ------------------------------------------------
+# Ex L01.1
+# ExL01.1 Compare the median, and the mean of the rainfall data
+print("Ex L01.1")
+
+
+# ------------------------------------------------
+# Ex L01.2
+# Compute the MAD, the IQR, and the standard deviation of the pressure data
+print("Ex L01.2")
+
+
+# ------------------------------------------------
+# Ex L01.3 a
+# Draw the empirical cumulative frequency distribution for the pressure data.
+print("Ex L01.3 a")
+
+
+# ------------------------------------------------
+# Ex L01.3 b
+# Compare it with a histogram of the same data.
+print("Ex L01.3 b")
+
+
+
+
+
